665dff5d1f74e06e9807901af2ac9571bc3974dd
Makwana <dilipmak@amazon.com>
2017-11-10 00:36:16 -0800

    user CRUD dao methods added

---
+++ server/patnet/src/patnet/model/Users.java
@@ -0,0 +1,114 @@
+package patnet.model;
+
+/**
+ * @author Dilip Makwana
+ *
+ */
+public class Users {
+	protected String Username;
+	protected String Password;
+	protected Type type;
+
+	public enum Type {
+		ORDINARY, ORGANIZATION, PHYSICIAN
+	}
+
+	protected int OrganizationId;
+	protected int PhysicianId;
+	protected String FirstName;
+	protected String LastName;
+
+	public Users(String username) {
+		super();
+		Username = username;
+	}
+
+	public Users(String username, String fn) {
+		super();
+		Username = username;
+		FirstName = fn;
+	}
+
+	public Users(String username, String password, Type type, int organizationId, int physicianId, String firstName,
+			String lastName) {
+		super();
+		Username = username;
+		Password = password;
+		this.type = type;
+		OrganizationId = organizationId;
+		PhysicianId = physicianId;
+		FirstName = firstName;
+		LastName = lastName;
+	}
+
+	public String getUsername() {
+		return Username;
+	}
+
+	public void setUsername(String username) {
+		Username = username;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	public int getOrganizationId() {
+		return OrganizationId;
+	}
+
+	public void setOrganizationId(int organizationId) {
+		OrganizationId = organizationId;
+	}
+
+	public int getPhysicianId() {
+		return PhysicianId;
+	}
+
+	public void setPhysicianId(int physicianId) {
+		PhysicianId = physicianId;
+	}
+
+	public String getUserName() {
+		return Username;
+	}
+
+	public void setUserName(String userName) {
+		this.Username = userName;
+	}
+
+	public String getFirstName() {
+		return Password;
+	}
+
+	public void setFirstName(String firstName) {
+		this.Password = firstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.LastName = lastName;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Users [Username=" + Username + ", Password=" + Password + ", OrganizationId=" + OrganizationId
+				+ ", PhysicianId=" + PhysicianId + ", FirstName=" + FirstName + ", LastName=" + LastName + "]";
+	}
+
+}
--- server/patnet/src/patnet/tools/RDStest.java
+++ server/patnet/src/patnet/tools/RDStest.java
@@ -6,10 +6,41 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import patnet.dal.ConnectionManager;
+import patnet.dal.UsersDao;
+import patnet.model.Users;
 
 public class RDStest {
 
 	public static void main(String[] args) throws SQLException {
+		testRDS();
+
+		testUsersDao();
+
+	}
+
+	static void testUsersDao() throws SQLException {
+
+		System.out.println("testing dao");
+
+		UsersDao usersDao = UsersDao.getInstance();
+
+		Users someUser = usersDao.getUserFromUserName("Organization");
+		System.out.println(someUser.toString());
+
+		Users newUser = new Users("dilip", "dilipPasswordNew", Users.Type.ORDINARY, 8, 1003037037, "dilipFname",
+				"dilipLname");
+
+		// usersDao.create(newUser);
+		// usersDao.delete(newUser);
+		usersDao.update(newUser);
+
+		System.out.println(newUser.toString());
+
+	}
+
+	static void testRDS() throws SQLException {
+
+		System.out.println("testing RDS ");
 
 		ConnectionManager connectionManager = new ConnectionManager();
 
@@ -20,9 +51,9 @@ public class RDStest {
 		try {
 			connection = connectionManager.getConnection();
 			selectStmt = connection.prepareStatement(selectHospitalSpending);
-			
+
 			results = selectStmt.executeQuery();
-			
+
 			if (results.next()) {
 				System.out.println("HospitalSpending count from RDS DB -> " + results.getInt("cnt"));
 			}
@@ -41,6 +72,7 @@ public class RDStest {
 			}
 		}
 
+		System.out.println("testing RDS end");
 	}
 
 }
