aa6ab7328be76d75587deff03f8d25e0ea3dae4c
ChienliMa <maqianlie@gmail.com>
2017-11-12 13:55:52 -0800

    draft OrgServlet

--- server/patnet/src/patnet/servlet/OrganizationServlet.java
+++ server/patnet/src/patnet/servlet/OrganizationServlet.java
@@ -1,6 +1,24 @@
 package patnet.servlet;
 
-import javax.servlet.*;
-public class OrganizationServlet extends HTTPServlet{
+import javax.servlet.http.HTTPservlet;
 
+import com.sun.net.ssl.internal.ssl.Provider;
+
+@Path(/Organization/{OrganizationId})
+public class OrganizationServlet extends HTTPServlet{
+	
+	@GET
+	public void getOrganization() {
+		
+	}
+
+	@POST
+	public void getOrganization() {
+		
+	}
+	
+	@CREATE
+	public void getOrganization() {
+		
+	}
 }
